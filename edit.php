3209030f2c12a2fbcebd57e3522974b22f364fa1
scruncho <60299363+scruncho@users.noreply.github.com>
2021-11-07 01:00:13 -0500

    Update edit.php

--- edit.php
+++ edit.php
@@ -58,7 +58,8 @@ else{
             <small id="phoneHelp" class="form-text text-muted">We'll never share your number with anyone else.</small>
         </div>
         
-        <button type="submit" name="submit" class="btn btn-success" class="btn-block" style="width:100%">Save Changes</button>
+        <a href="viewrecords.php" class="btn btn-default" >Back to List</a>
+        <button type="submit" name="submit" class="btn btn-success" >Save Changes</button>
 </form>
 
 <?php } ?>
